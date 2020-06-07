3feade6fc1ac133895dd296f2d5c7a22e8e79037
Igor Matuszewski <igor@sequoia-pgp.org>
2020-06-07 23:48:07 +0200

    Remove unused list_algorithms in helpers.rs

--- src/helpers.rs
+++ src/helpers.rs
@@ -4,21 +4,3 @@ pub mod blob;
 pub use blob::{Blob, BlobLayout};
 pub mod string;
 pub use string::WideCString;
-
-/*pub fn list_algorithms() {
-    let mut alg_count = MaybeUninit::<ULONG>::uninit();
-    let mut alg_list = MaybeUninit::<*mut BCRYPT_ALGORITHM_IDENTIFIER>::uninit();
-    unsafe {
-        BCryptEnumAlgorithms(
-            BCRYPT_HASH_OPERATION,
-            alg_count.as_mut_ptr(),
-            alg_list.as_mut_ptr(),
-            0
-        );
-
-        for i in 0..alg_count.assume_init() {
-            let name = WideCString::from_ptr((*alg_list.assume_init().offset(i as isize)).pszName);
-            println!("{}", name.to_str());
-        }
-    }
-}*/
