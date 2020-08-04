2c093d131d9e12b7e60c882b4c53f8107d5f4f8a
Igor Matuszewski <igor@sequoia-pgp.org>
2020-08-04 17:34:36 +0200

    Remove commented out list_algorithms
    It's currently commented out and unused but would be good to see it
    return at some point.

--- src/helpers.rs
+++ src/helpers.rs
@@ -181,24 +181,6 @@ impl ToString for WindowsString {
     }
 }
 
-/*pub fn list_algorithms() {
-    let mut alg_count = MaybeUninit::<ULONG>::uninit();
-    let mut alg_list = MaybeUninit::<*mut BCRYPT_ALGORITHM_IDENTIFIER>::uninit();
-    unsafe {
-        BCryptEnumAlgorithms(
-            BCRYPT_HASH_OPERATION,
-            alg_count.as_mut_ptr(),
-            alg_list.as_mut_ptr(),
-            0
-        );
-
-        for i in 0..alg_count.assume_init() {
-            let name = WindowsString::from_ptr((*alg_list.assume_init().offset(i as isize)).pszName);
-            println!("{}", name.to_str());
-        }
-    }
-}*/
-
 /// A typed view into an opaque blob of heap-allocated bytes.
 pub struct TypedBlob<T: ?Sized> {
     allocation: Box<[u8]>,
